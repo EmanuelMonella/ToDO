ef820e4021bebf4ad61ab287a45b1241b99b0257
EmanuelMonella <emanuel.monella@alumnos.ispi4038.edu.ar>
2024-09-04 18:19:32 -0300

    creacion archivo ventana

--- ToDo.py
+++ ToDo.py
@@ -1,33 +1,6 @@
-from PySide6 import QtWidgets, QtCore
-from PySide6.QtGui import QStandardItem, QStandardItemModel
-from recursos.todo_grafico import Ui_ToDo
+from PySide6 import QtWidgets
 
-class VentanaToDo(QtWidgets.QWidget):
-    def __init__(self):
-        super().__init__()
-        self.__ui = Ui_ToDo()
-        self.__ui.setupUi(self)
-
-        self.__modelo = QStandardItemModel()
-        self.__modelo.setHorizontalHeaderLabels(['Realizada', 'Tarea'])
-        self.__ui.tableView.setModel(self.__modelo)
-
-    def __agregar_tarea(self):
-        tarea_nueva = self.__ui.nueva_tarea.text()
-        if tarea_nueva:
-            checkbox_item = QStandardItem()
-            checkbox_item.setCheckable(True)
-            checkbox_item.setCheckState(QtCore.Qt.Unchecked)
-
-            item_de_tarea = QStandardItem(tarea_nueva)
-
-            self.__modelo.appendRow([checkbox_item, item_de_tarea])
-            self.__ui.nueva_tarea.clear()
-
-
-    def __eliminar_tarea(self):
-        fila_seleccionada = self.__ui.tableView.currentIndex().row()
-        self.__modelo.removeRow(fila_seleccionada)
+from recursos.ventanas import VentanaToDo
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication()
